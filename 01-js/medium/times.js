41d51612bf39866d733768274f7eb0241dbacf35
Aryan Singla <smthng.shady@gmail.com>
2023-06-12 12:24:52 +0530

    JS - Medium Done

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,16 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
+    let time1 = new Date().getTime();
+    let sum=0;
+    for(let i =1; i<n+1; i++) {
+        sum+=i;
+    }
+    let time2 = new Date().getTime();
+    console.log( ((time2-time1)/1000) + 'sec');
     return 0.01;
 }
+
+// calculateTime(100);
+// calculateTime(100000);
+// calculateTime(1000000000);
