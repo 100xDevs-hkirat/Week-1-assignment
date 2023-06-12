596b45502397f8b558500b74ae5af84e2c9c9d44
jayakrishna855 <69287549+jayakrishna855@users.noreply.github.com>
2023-06-12 20:56:38 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,8 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length !== str2.length) return false;
+  return (str1.split('').sort().join('') == str2.split('').sort().join(''));
 }
 
 module.exports = isAnagram;
