dfe428de82f26ab69b85f268ad3c1274080c7504
Amit Dutta <amitdutta2406@gmail.com>
2023-06-12 15:29:52 +0530

    Added solutions to medium and hard prolems

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,38 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo);
+  }
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1);
+  }
+  update(index, updatedTodo) {
+    this.todos[index] = updatedTodo;
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    return this.todos[indexOfTodo];
+  }
+  clear() {
+    this.todos = [];
+  }
 }
+
+let todo = new Todo();
+todo.add('Buy milk');
+todo.add('Buy eggs');
+todo.add('Buy bread');
+console.log(todo.getAll());
+todo.remove(1);
+console.log(todo.getAll());
+todo.update(1, 'Buy cheese');
+console.log(todo.getAll());
+console.log(todo.get(1));
 
 module.exports = Todo;
