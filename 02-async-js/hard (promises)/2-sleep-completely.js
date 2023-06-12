6dd4aa9ac65d7c6a521d501a3b26e6331c335c6b
manoj-gowda-hs <128355539+manoj-surya@users.noreply.github.com>
2023-06-12 12:19:04 +0530

    solved 2-sleep-completely.js

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,13 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  const startTime = new Date().getTime();
+  while (new Date().getTime() - startTime < seconds*1000) {
+    
+  }
 }
+
+console.log('Start timer');
+sleep(5);
+console.log('Stop timer);
