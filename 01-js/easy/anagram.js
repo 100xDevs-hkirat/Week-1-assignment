6c71089577a8aa6e2a9ed3da466a504a1f6c4ed3
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-12 19:39:40 +0530

    Anagram Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,26 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
+const solve = (str1, str2) => {
+	str1 = str1.toLowerCase();
+	str2 = str2.toLowerCase();
+	const m = new Set();
 
+	for (let i of str1) {
+		m.add(i);
+	}
+
+	for (let i of str2) {
+		if (m.has(i)) {
+			m.delete(i);
+		}
+	}
+
+	return m.size == 0;
+};
 function isAnagram(str1, str2) {
-
+	if (str1.length > str2.length) return solve(str1, str2);
+	return solve(str2, str1);
 }
 
 module.exports = isAnagram;
