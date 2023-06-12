1de013572242a97adf180dedee85236f2864d374
CS Sahil <xehil.xyz@gmail.com>
2023-06-12 23:49:33 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,13 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let sum = 0;
+  const startTime = new Date();
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  const endTime = new Date();
+  return (endTime - startTime) / 1000;
 }
+
+//console.log(calculateTime(1000000000));
