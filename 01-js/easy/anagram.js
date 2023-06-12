a7e392296c54e10fb3e7ca413c787249c6b94b29
Bikash Krishna Panga <115647984+BIKASHKRISHNAPANGA@users.noreply.github.com>
2023-06-12 23:25:51 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+ var k=0;
+  if(str1.length===str2.length)
+  {
+  for(var i=0;i<str1.length;i++)
+  {
+    for(var j=0;j<str2.length;j++)
+  {
+    if(str1.charAt(i)===str2.charAt(j))
+      k++;
+  }
+  }
+    if(k===str1.length)
+      return (true);
+    else
+      return(false);
+  }
+  else
+      return(false);
 }
 
 module.exports = isAnagram;
