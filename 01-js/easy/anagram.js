353f81803d07ade2ca381b0752104e83e20641a8
Archit Sehgal <68142222+archit-sehgal@users.noreply.github.com>
2023-06-12 22:37:49 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,21 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !==str2.length){
+          return false
+      }
+      var str1lower=str1.toLowerCase();
+      var str2lower=str2.toLowerCase();
 
-}
+      str1lower=str1lower.split("").sort().join();
+      str2lower=str2lower.split("").sort().join();
+
+      if(str1lower===str2lower){
+          return true;
+      }
+      else{
+          return false;
+  }
+  console.log(isAnagram("str1","str2"))
 
 module.exports = isAnagram;
