9f168b8e31b72c3839a5176fd7a719444d804105
manoj-gowda-hs <128355539+manoj-surya@users.noreply.github.com>
2023-06-12 12:39:18 +0530

    solved 1-promisify-setTimeout.js

--- 02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,5 +2,15 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
+
 function wait(n) {
+    return new Promise((resolve,reject)=>{
+        if(n%2==0)
+           setTimeout(resolve,n*1000)
+        else
+           setTimeout(reject,n*1000)
+    });
 }
+
+wait(3).then(()=> console.log('the number of seconds passed is even'))
+       .catch(()=> console.log('the number of seconds passed is odd '))
