a1169d065e3ee013d9a57e8219838458cff0bfd6
bhaskarConsultant <bhaskar.consultant_eng@jukshio.com>
2023-06-13 12:13:21 +0530

    changes in the 02 exercises section

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,36 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("1"), 1000);
+  });
+  return answer;
 }
 
 function waitTwoSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("2"), 2000);
+  });
+  return answer;
 }
 
 function waitThreeSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("3"), 3000);
+  });
+  return answer;
 }
 
 function calculateTime() {
-
+  const startTime = new Date().getTime();
+  Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()]).then(
+    (values) => {
+      console.log(values);
+      const endTime = new Date().getTime();
+      console.log(Math.abs(endTime - startTime) / 1000);
+    }
+  );
 }
+
+calculateTime();
--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -4,19 +4,40 @@
  * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
-
 function waitOneSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("1"), 1000);
+  });
+  return answer;
 }
 
 function waitTwoSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("2"), 2000);
+  });
+  return answer;
 }
 
 function waitThreeSecond() {
-
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("3"), 3000);
+  });
+  return answer;
 }
 
 function calculateTime() {
-
+  const startTime = new Date().getTime();
+  waitOneSecond().then((value) => {
+    console.log(value);
+    waitTwoSecond().then((value) => {
+      console.log(value);
+      waitThreeSecond().then((value) => {
+        console.log(value);
+        const endTime = new Date().getTime();
+        console.log(Math.abs(endTime - startTime) / 1000);
+      });
+    });
+  });
 }
+
+calculateTime();
