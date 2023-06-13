c9e33a835df2dac757091ca5b1611126b2b512c0
Adarsh <pandey.adarshking@gmail.com>
2023-06-13 12:13:17 +0530

    anagram done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,21 @@
 */
 
 function isAnagram(str1, str2) {
+  function sorti(Stringg) {
+    return Stringg.split("").sort().join("");
+  }
+  let a = sorti(str1);
+  let b = sorti(str2);
 
+  for (let i = 0; i <= str1.length; i++) {
+    if (a.charAt(i) !== b.charAt(i)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+
+
 
 module.exports = isAnagram;
