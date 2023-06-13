fb83806f04c18b1534803a8a30a37ceb8a4e5ce3
RaviiiiYadav <1947ry@gmail.com>
2023-06-13 22:46:27 +0530

    Assignment of week 1

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -1,13 +1,15 @@
-/*
-  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
-  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
-
-  Once you've implemented the logic, test your code by running
-  - `npm run test-palindrome`
-*/
-
 function isPalindrome(str) {
-  return true;
+  str=str.toLowerCase();
+  var final=str.split('').reverse().join('');
+  if(final===str){
+    return true 
+  }
+  else{
+    return false
+  }
+  
 }
+
+
 
 module.exports = isPalindrome;
