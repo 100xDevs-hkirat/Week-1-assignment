7bfd274d4c1c2576602e3a96d2912db16ad83507
Mahesh katakam <maheshkatakam@Maheshs-MacBook-Air.local>
2023-06-13 21:20:57 +0530

    clock added

---
+++ 02-async-js/medium/2-clock.js
@@ -0,0 +1,19 @@
+const displayTime = () => {
+  console.clear();
+  let currentDate = new Date();
+  let utc = currentDate.toLocaleTimeString([], { hour12: false });
+  utc = [...utc.slice(0, 5), ":", utc.slice(5)].join("");
+  console.log(utc);
+  let locale = currentDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digits",
+    second: "2-digits",
+  });
+  locale = [...locale.slice(0, 5), ":", locale.slice(5)].join("");
+  console.log(locale);
+};
+
+setInterval(displayTime, 1000);
+
+// adding test3
+// soacls
