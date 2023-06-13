1f05faa3f4deb9933c1268293053fae02053c971
bhaskarConsultant <bhaskar.consultant_eng@jukshio.com>
2023-06-13 13:45:11 +0530

    completed the assignment

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -32,7 +32,11 @@ class Calculator {
     this.result *= number;
   }
   divide(number) {
-    this.result /= number;
+    if (number !== 0) {
+      this.result /= number;
+    } else {
+      throw err;
+    }
   }
   clear() {
     this.result = 0;
@@ -40,7 +44,21 @@ class Calculator {
   getResult() {
     return this.result;
   }
-  calculate(str) {}
+  calculate(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === " ") {
+        continue;
+      } else {
+        result += str[i];
+      }
+    }
+    if (result.includes("/0")) {
+      throw new Error("Cannot divide with zero");
+    }
+    let result1 = result.replace(/[^-()\d/*+.]/g, "");
+    this.result = eval(result1);
+  }
 }
 
 module.exports = Calculator;
