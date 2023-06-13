568b4d1a19452cf5162427a8af40320e540dd32a
Sid <126329622+siddnegi@users.noreply.github.com>
2023-06-13 21:46:05 +0530

    Completed anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,40 @@
 */
 
 function isAnagram(str1, str2) {
+  // Convert both strings to lowercase
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  // Remove whitespace from both strings
+  str1 = str1.replace(/\s/g, '');
+  str2 = str2.replace(/\s/g, '');
+
+  // Check if the lengths of the strings are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // Convert strings to arrays and sort them alphabetically
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  // Compare the sorted strings
+  if (sortedStr1 === sortedStr2) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+// Example usage:
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (isAnagram(string1, string2)) {
+  console.log('The strings are anagrams.');
+} else {
+  console.log('The strings are not anagrams.');
+}
+
 
 module.exports = isAnagram;
