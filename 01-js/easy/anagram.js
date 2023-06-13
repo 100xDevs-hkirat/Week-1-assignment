0042194b799159b98c8d5dcae8443624e2a4772a
Bhagavan Marpadaga <bhagavan.m@lsq.net>
2023-06-13 08:30:00 +0530

    anagrams is solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,30 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1=str1.toLowerCase()
+  str2=str2.toLowerCase()
+  const map=new Map();
+  for(let i=0;i<str1.length;i++){
+    if(map.has(str1[i])){
+      map.set(str1[i],map.get(str1[i])+1)
+    }else{
+      map.set(str1[i],1)
+    }
+  }
+  for(let i=0;i<str2.length;i++){
+    if(map.has(str2[i])){
+      map.set(str2[i],map.get(str2[i])-1)
+    }else{
+      return false
+    }
+    if(map.get(str2[i])==0){
+      map.delete(str2[i])
+    }
+  }
+  if(map.size>0){
+    return false
+  }
+  return true
 }
 
 module.exports = isAnagram;
