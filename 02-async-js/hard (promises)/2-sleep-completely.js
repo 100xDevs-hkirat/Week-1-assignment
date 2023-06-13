6e4cea67a3e706d8be5577beef3aa7aa2929b295
bhaskarConsultant <bhaskar.consultant_eng@jukshio.com>
2023-06-13 12:25:26 +0530

    completed all easy, medium, hard 02 exercises

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,15 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
+function sleep(seconds) {
+  const answer = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`Waited for ${seconds} seconds`), seconds * 1000);
+  });
 
+  return answer;
 }
+
+sleep(5).then((value) => {
+  console.log(value);
+  console.log("Executed after waiting");
+});
