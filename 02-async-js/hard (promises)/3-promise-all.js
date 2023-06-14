de778c1b95335047db6e7487e1f0ad9ef67f1016
asbar <asbar03KK@gmail.com>
2023-06-14 18:47:50 +0530

    compleated hard of 02-js

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -3,20 +3,44 @@
  * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
  * Print how long it took for all 3 promises to resolve.
  */
-
+function promiseBody1(resolve) {
+  setTimeout(resolve, 1000);
+}
+function promiseBody2(resolve) {
+  setTimeout(resolve, 2000);
+}
+function promiseBody3(resolve) {
+  setTimeout(resolve, 3000);
+}
+// function promiseBody1(resolve){
+//     setTimeout(resolve,1000);
+// }
+let promise1 = new Promise(promiseBody1);
+let promise2 = new Promise(promiseBody2);
+let promise3 = new Promise(promiseBody3);
 
 function waitOneSecond() {
-
+  return promise1;
 }
 
 function waitTwoSecond() {
-
+  return promise2;
 }
 
 function waitThreeSecond() {
-
+  return promise3;
 }
 
 function calculateTime() {
+  let start = Date.now();
+  let promiseAll = Promise.all([promise1, promise2, promise3]);
 
+  promiseAll.then(() => {
+    let end = Date.now();
+    console.log(`Promise All Time: ${(end - start) / 1000}`);
+  });
 }
+waitOneSecond().then(() => console.log("Finished 1"));
+waitTwoSecond().then(() => console.log("Finished 2"));
+waitThreeSecond().then(() => console.log("Finished 3"));
+calculateTime();
--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -4,19 +4,40 @@
  * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
+function promiseBody1(resolve) {
+  setTimeout(resolve, 1000);
+}
+function promiseBody2(resolve) {
+  setTimeout(resolve, 2000);
+}
+function promiseBody3(resolve) {
+  setTimeout(resolve, 3000);
+}
 
 function waitOneSecond() {
-
+  return new Promise(promiseBody1);
 }
 
 function waitTwoSecond() {
-
+  return new Promise(promiseBody2);
 }
 
 function waitThreeSecond() {
-
+  return new Promise(promiseBody3);
 }
 
 function calculateTime() {
-
+  let start = Date.now();
+  waitOneSecond().then(() => {
+    console.log("finished 1");
+    waitTwoSecond().then(() => {
+      console.log("finished 2");
+      waitThreeSecond().then(() => {
+        console.log("finished 3");
+        let end = Date.now();
+        console.log(`time: ${end - start}`);
+      });
+    });
+  });
 }
+calculateTime();
