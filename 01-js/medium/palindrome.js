74eb94ef19af924142750d1518e86734c2c89ddc
asbar <asbar03KK@gmail.com>
2023-06-14 18:42:09 +0530

    compleated easy and medium of 01-js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,31 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function hasSpecialCharacters(str) {
+  str = str.toLowerCase();
+  const regex = /[!@#$%^&*(),.?":{}|<>\s]/g;
+  str = str.replace(regex, "");
+
+  return str;
+}
 
 function isPalindrome(str) {
+  str = hasSpecialCharacters(str);
+  if (str.length <= 2) {
+    return true;
+  } else {
+    for (let i = 0; i < str.length / 2; i++) {
+      if (!(str[i] === str[str.length - i - 1])) {
+        return false;
+      }
+    }
+  }
   return true;
 }
+// let str = "abdA  ba!  ";
+// const regex = /[!@#$%^&*(),.?":{}|<>\s]/g;
+// str = str.replace(regex, "");
+const ans = isPalindrome("abaA  ba!  ");
+console.log(ans);
 
 module.exports = isPalindrome;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -6,7 +6,24 @@ Try running it for
 3. Sum from 1-1000000000
 Hint - use Date class exposed in JS
 */
+function sum(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    count += i;
+  }
+  return count;
+}
 
 function calculateTime(n) {
-    return 0.01;
+  let date = new Date();
+  let start = date.getTime();
+  //   console.log(start);
+  let sumOfNumbers = sum(n);
+  let end = Date.now();
+  //   console.log(end);
+  let time = end - start;
+  //   console.log(time);
+  return time / 1000;
 }
+let caltime = calculateTime(1000000000);
+console.log(caltime);
