f6c21d7267e8b4e3dc9ff48c1dff8caae7c50131
jayakrishna855 <69287549+jayakrishna855@users.noreply.github.com>
2023-06-14 07:12:43 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,7 +9,7 @@
 
 function isAnagram(str1, str2) {
   if(str1.length !== str2.length) return false;
-  return (str1.split('').sort().join('') == str2.split('').sort().join(''));
+  return (str1.toLowerCase().split('').sort().join('') == str2.toLowerCase().split('').sort().join(''));
 }
 
 module.exports = isAnagram;
