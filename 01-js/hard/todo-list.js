1dab919d7fa3e2e17b39f192853c8d71c9c850ee
Vishwa <109414918+vishwaTj@users.noreply.github.com>
2023-06-14 23:02:33 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,37 @@
 */
 
 class Todo {
+    todo=[]
 
+        add(task){
+          this.todo.push(task);
+          return todo;
+        }
+
+        update(task,value){
+            for(let i=0;i<this.todo.length;i++){
+              if(this.todo[i]==task){
+                this.todo[i]=value;
+              }
+            }
+            return todo;
+        }
+
+        getAll(){
+           for(let i=0;i<this.todo.length;i++){
+              console.log(this.todo[i]);
+            }
+            return todo
+        }
+
+        get(index){
+            return this.todo[index];
+        }
+
+        clear(){
+          let n = this.todo.length;
+          this.todo.splice(0,n);
+        }
 }
 
 module.exports = Todo;
