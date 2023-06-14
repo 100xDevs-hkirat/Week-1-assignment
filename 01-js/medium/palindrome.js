a35b2964378a7ffbe4ab49377f6b0ebafb0dba68
Yash Mishra <yashmishra3297@gmail.com>
2023-06-14 11:51:36 +0530

    Sloved Easy,Medium & hard problems for Week 1 assignment

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,9 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  const normalizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversedStr = normalizedStr.split('').reverse().join('');
+  return normalizedStr === reversedStr;
 }
 
 module.exports = isPalindrome;
