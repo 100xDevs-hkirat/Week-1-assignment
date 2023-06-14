6a3e2582a0b979e1bf8fac4d36d805ed056a65aa
Kushal Srivastava <134607390+kushsriv@users.noreply.github.com>
2023-06-14 19:37:42 +0530

    Updated 01- anagram.js
    All TESTS PASSED!!

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,45 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
 
+function isAnagram(str1, str2) {
+  if(str1.length==str2.length)
+  {
+    str1=str1.toLowerCase();
+    str2=str2.toLowerCase();
+  let s=[];
+  let f=[];
+ for(let i=0;i<str1.length;i++)
+ {
+  if(str1.charAt(i)!="")
+s.push(str1.charAt(i));
+ }
+ for(let j=0;j<str2.length;j++)
+ {
+  if(str2.charAt(j)!="")
+f.push(str2.charAt(j))
+ }
+ s=s.sort();
+ f=f.sort();
+ let c=0;
+ for(let g=0;g<str1.length;g++)
+ {
+  if(s[g]==f[g])
+  c++;
+ }
+ 
+ if(c==str1.length)
+ return true 
+ else return false;
+  
 }
+else
+return false
+  
+}
+
+
+const x= isAnagram("GOLU", "Golu");
+console.log(x);
 
 module.exports = isAnagram;
