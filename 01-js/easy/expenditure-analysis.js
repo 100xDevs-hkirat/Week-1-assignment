7cad6223fad615202bb25d5a3d517127da164683
Kushal Srivastava <134607390+kushsriv@users.noreply.github.com>
2023-06-14 19:40:57 +0530

    Updated 01- expenditure-analysis.js
    All TESTS PASSED!!

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,38 @@
   - `npm run test-expenditure-analysis`
 */
 
+const transactions=[ { itemName : "Hide and Seek", category : "food", price : 60, timestamp : new Date().getTime() },
+{ itemName : "Maggi", category : "food", price : 24, timestamp : new Date().getTime() },
+{ itemName : "T-Shirt", category : "clothing", price : 650, timestamp : new Date().getTime() },
+{ itemName : "Shirt", category : "clothing", price : 2800, timestamp : new Date().getTime() },
+{ itemName : "Iphone", category : "electronics", price : 80000, timestamp : new Date().getTime() }]
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+    var sz = transactions.length;
+    var uniqueList = [];
+    var list = [];
+    for(var i=0; i<sz; i++) {
+      var object = transactions[i];
+      if(!uniqueList.includes(object.category)){
+          // not found
+          uniqueList.push(object.category);
+          list.push({category: object.category, totalSpent: object.price});
+      }
+      else {
+        // found
+        var foundObject = list.find(obj => obj.category === object.category);
+  
+        foundObject.totalSpent += object.price;
+      }
+    }
+    return list;
+  
+  }
+  
+  const x=calculateTotalSpentByCategory(transactions);
+  console.log(x);
+
+  
+  
 
 module.exports = calculateTotalSpentByCategory;
