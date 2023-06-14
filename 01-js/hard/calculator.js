e18bb3851956df6f998615f76a95fda83a6ce85c
asbar <asbar03KK@gmail.com>
2023-06-14 18:46:08 +0530

    compleated hard of 01-js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,7 +16,107 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-calculator`
 */
-
-class Calculator {}
-
+function operatorGuess(op) {
+  if (op === "+") {
+    return "add";
+  }
+}
+class Calculator {
+  constructor(result) {
+    this.result = result || 0;
+  }
+  add(num1) {
+    // if (num1 && num2) {
+    //   this.result = num1 + num2;
+    // } else {
+    //   if (num2 == null) {
+    //     this.result = num1;
+    //   }
+    //   this.result = num2;
+    // }
+    this.result += num1;
+  }
+  subtract(num1) {
+    // if (num1 && num2) {
+    //   this.result = num1 - num2;
+    // } else {
+    //   if (num2 == null) {
+    //     this.result = -num1;
+    //   }
+    //   this.result = -num2;
+    // }
+    this.result -= num1;
+  }
+  multiply(num1) {
+    // if (num1 && num2) {
+    //   this.result = num1 * num2;
+    // }
+    // if (num1 < 0 && num2 < 0) {
+    //   this.result = num1 * num2;
+    // }
+    // if (num1 < 0 || num2 < 0) {
+    //   this.result = -(num1 * num2);
+    // } else {
+    //   this.result = 0;
+    // }
+    this.result *= num1;
+  }
+  divide(num1) {
+    // if (num1 && num2) {
+    //   this.result = num1 / num2;
+    // }
+    if (num1 === 0) {
+      throw new Error();
+    } else {
+      this.result /= num1;
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(str) {
+    const regx1 = /[abcdefghijklmnopqrstuvwxyz!@#$%^&,<>?:;'"|~`_=]/;
+    const regx2 = /\s/g;
+    if (regx1.test(str)) {
+      throw new Error();
+    }
+    str = str.toLowerCase();
+    str = str.replace(regx2, "");
+    // try {
+    //   this.result = eval(str);
+    // } catch (error) {
+    //   throw error;
+    // }
+    this.result = eval(str);
+    if (!isFinite(this.result)) {
+      throw Error();
+    }
+  }
+  // isOperator(car) {
+  //   const reg = /[+-*/]/;
+  //   return reg.test(car);
+  // }
+  // bracketCal(start, end, str) {
+  //   for (let i = start; i < end; i++) {
+  //     if (this.isOperator(str[i])) {
+  //       let op = operatorGuess(str[i]);
+  //     }
+  //     let num1 = str;
+  //   }
+  // }
+}
+// const regx1 = /[abcdefghijklmnopqrstuvwxyz!@#$%^&,.<>?:;'"|~`_=]/;
+// let str = "10 +   2 *    (   6 - (4 + 1) / 2) + ab!";
+// if (regx1.test(str)) {
+//   console.log(Error);
+// }
+// let str = "10 +   2 *    (   6 - (4 + 1) / 2) + 7";
+// const regx2 = /\s/g;
+// str = str.toLowerCase();
+// str = str.replace(regx2, "");
+// console.log(str);
+// let index = str.indexOf()
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,33 @@
 */
 
 class Todo {
-
+  todos = [];
+  add(str) {
+    this.todos.push(str);
+  }
+  remove(index) {
+    if (index < this.todos.length) {
+      this.todos.splice(index, 1);
+    }
+  }
+  update(index, updatedTodo) {
+    if (index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } else {
+      return null;
+    }
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
