da9ea79ff6b2b583c5420c4be7beaf86c221797b
Nitin Kalra <nikkalra88@gmail.com>
2023-06-14 19:13:24 +0530

    01-js: easy: anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,6 +7,16 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+	if (str1.length !== str2.length) return false;
+
+	for (let i = 0; i < str1.length; i++) {
+		if (!str2.toLowerCase().includes(str1[i].toLowerCase())) {
+			return false;
+		}
+		str2 = str2.replace(str1[i], "");
+	}
+	return true;
+}
 
 module.exports = isAnagram;
