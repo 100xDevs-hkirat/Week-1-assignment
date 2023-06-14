8e17f954de5ac6b5ef40a8158bd7561cc1449f03
jayakrishna855 <69287549+jayakrishna855@users.noreply.github.com>
2023-06-14 21:26:37 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,36 @@
 */
 
 class Todo {
-
+ constructor(){
+    this.todos = [];
+ }
+ add = (todo)=>{
+    this.todos.push(todo);
+ }
+ remove = (indexOfTodo)=>{
+    if(indexOfTodo < this.todos.length){
+        this.todos.splice(indexOfTodo,1);
+    }
+    
+ }
+ update = (index, updatedTodo)=>{
+    if(index < this.todos.length){
+        this.todos[index] = updatedTodo;
+    }
+    
+ }
+ getAll = ()=>{
+    return this.todos;
+ }
+ get = (indexOfTodo)=>{
+    if(indexOfTodo >= this.todos.length){
+        return null;
+    }
+    return this.todos[indexOfTodo];
+ }
+ clear = ()=>{
+    this.todos.length = 0;
+ }
 }
 
 module.exports = Todo;
