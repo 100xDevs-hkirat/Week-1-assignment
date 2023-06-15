dcc4b021560a368699dc449789452e6980f7d8ee
Sid <126329622+siddnegi@users.noreply.github.com>
2023-06-15 19:30:48 +0530

    Completed times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,18 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
-}
+    const startTime = new Date();
+  
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  
+    const endTime = new Date();
+    const totalTime = (endTime - startTime) / 1000; // Convert milliseconds to seconds
+  
+    return totalTime;
+  }
+  console.log(calculateTime(100));
+  console.log(calculateTime(100000));
+  console.log(calculateTime(1000000000));
