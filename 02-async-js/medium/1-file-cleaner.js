3807f8e1b3d09e426abc42c05ae82f232ba4dae7
Arshad-Aly <132923076+Arshad-Aly@users.noreply.github.com>
2023-06-15 15:43:59 +0530

    Completed 1-file-cleaner.js

--- 02-async-js/medium/1-file-cleaner.js
+++ 02-async-js/medium/1-file-cleaner.js
@@ -1,1 +1,24 @@
+const fs = require('fs')
 
+
+fs.readFile('./1-file-cleaner.md' , 'utf8' , (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+
+    console.log(data)
+
+    const Data = data.replace(/\s+/g, " ")
+
+    console.log("Content Without Extra Spaces:", Data)
+    
+    fs.appendFile('./1-file-cleaner.md', Data , 'utf8' , (err) => {
+        if(err) {
+            console.error('Error Writing to file:' , err)
+        }
+    
+    })
+
+    
+})
