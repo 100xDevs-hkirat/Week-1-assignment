9ff979c636d219b5bff3a763a1e9b598f0868e62
Somnath Golui <code.somu@gmail.com>
2023-06-15 10:33:02 +0530

    Wrote both methods readFile and createReadStream

--- 02-async-js/easy/read_file.js
+++ 02-async-js/easy/read_file.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 
+// fs.readFile("/var/log/syslog", "utf-8", (error, data) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log(data);
+// })
+
 const stream = fs.createReadStream("/var/log/syslog", "utf-8");
 
 stream.on("data", (chunk) => {
