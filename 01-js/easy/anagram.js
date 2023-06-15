742e3c4233fd2dfb23bd8cf81bafe293c462385a
Akash_gahlot <akashgahlot1926@gmail.com>
2023-06-15 16:27:34 +0530

    completed easy and mediun problems

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase();
+  str2.str2.toLowerCase();
+  let match = false;
+  for (let i = 0; i < str1.length; i++) {
+    match = false;
+    for (let j = 0; j < str2.length; j++) {
+      if (str1.charAt(i) == str2.charAt(j)) {
+        match = true;
+        break;
+      }
+    }
+  }
+  if (match) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log("Is anagram :" + isAnagram("spar", "rasp"));
 module.exports = isAnagram;
