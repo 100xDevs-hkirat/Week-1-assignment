2270778c1c7f8f3ff2a525dbfa3cc25a62338001
Siddiksid <siddiksid15@gmail.com>
2023-06-15 17:48:51 +0530

    Update anagram.js
    solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,13 @@
 */
 
 function isAnagram(str1, str2) {
-
+str1 = str1.split("").sort().join("");
+  str2 = str2.split("").sort().join("");
+  if (str1 == str2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
