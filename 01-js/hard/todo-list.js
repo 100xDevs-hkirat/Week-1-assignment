fc8a92f45923f70b54ca811ce1a0b5ee3fc5c3df
Somnath Golui <code.somu@gmail.com>
2023-06-15 11:15:53 +0530

    todo-list.js passes all test cases

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,48 @@
 */
 
 class Todo {
+  constructor() {
+    this.listOfTodos = [];
+  }
+  add(todo) {
+    this.listOfTodos.push(todo);
+  }
 
+  remove(index) {
+    try {
+      if (index >= 0 && index < this.listOfTodos.length) {
+        this.listOfTodos.splice(index, 1);
+      }
+    } catch (error) {
+      throw error.message("Index out of bound");
+    }
+  }
+
+  update(index, updatedTodo) {
+    try {
+      if (index >= 0 && index < this.listOfTodos.length) {
+        this.listOfTodos[index] = updatedTodo;
+      }
+    } catch (error) {
+      return error.message("Invalid Task");
+    }
+  }
+
+  getAll() {
+    return this.listOfTodos;
+  }
+
+  get(index) {
+    if (index >= 0 && index < this.listOfTodos.length) {
+      return this.listOfTodos[index];
+    } else {
+      return null;
+    }
+  }
+
+  clear() {
+    this.listOfTodos = [];
+  }
 }
 
 module.exports = Todo;
