1cc3f6eb3f94cddf7d94bb86769f85d9c1a6e560
Akash_gahlot <akashgahlot1926@gmail.com>
2023-06-15 17:53:53 +0530

    toDo assignment completed

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,64 @@
 */
 
 class Todo {
+  constructor() {
+    this.tasks = [];
+  }
 
+  //function to add Todo in todos list
+  add = (toDo) => {
+    this.tasks.push(toDo);
+    console.log(JSON.stringify(this.tasks));
+  };
+  //function to remove the toDo via index value
+  remove = (index) => {
+    this.tasks.splice(index, 1);
+    console.log(JSON.stringify(this.tasks));
+  };
+  //function to update particular todo via index value
+  updateToDo = (index, toDo) => {
+    this.tasks[index] = toDo;
+    console.log(JSON.stringify(this.tasks));
+  };
+  //function to get all toDo from the list
+  getAll = () => {
+    return JSON.stringify(this.tasks);
+  };
+  //function to get all toDo from the list via index value of toDo.
+  gettoDo = (indexOfTodo) => {
+    return this.tasks[indexOfTodo];
+  };
+  ///function to empty the to do List.
+  clear = () => {
+    this.tasks = [];
+    console.log(JSON.stringify(this.tasks));
+  };
 }
 
-module.exports = Todo;
+const obj = new Todo();
+
+//function to add Todo in todos list
+obj.add("to do programming");
+//function to add Todo in todos list
+obj.add("to watch movie");
+//function to add Todo in todos list
+obj.add("to learn DSA");
+//function to add Todo in todos list
+obj.add("to play footbal");
+
+//function to remove the toDo via index value
+obj.remove(1);
+
+//function to update particular todo via index value
+obj.updateToDo(3, "to play chess");
+
+//function to get all toDo from the list
+obj.getAll();
+
+//function to get  toDo from the list via index value of toDo
+obj.gettoDo(2);
+
+//function to empty the to do List
+obj.clear();
+
+// module.exports = Todo;
