e1cdec491834a7511c8ebbea2e777dd9f4e71c08
Arshad-Aly <132923076+Arshad-Aly@users.noreply.github.com>
2023-06-15 15:27:29 +0530

    Completed expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,39 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryList = {}
+  
+  for(const transaction of transactions){
+    const {category, price} = transaction
+
+
+    if(category in categoryList) {
+      categoryList[category] += price
+    } else {
+      categoryList[category] = price
+    }
+  }
+
+
+  const result =  [];
+  for(const category in categoryList){
+    const Totalamountspend = categoryList[category]
+    const categoryObject = { [category] : Totalamountspend }
+    result.push(categoryObject)
+  }
+
+  return result
 }
 
 module.exports = calculateTotalSpentByCategory;
+
+const transactions = [
+  { itemName: "Item 1", category: "Category A", price: 10, timestamp: "2023-01-01" },
+  { itemName: "Item 2", category: "Category B", price: 15, timestamp: "2023-01-02" },
+  { itemName: "Item 3", category: "Category A", price: 5, timestamp: "2023-01-03" },
+  { itemName: "Item 4", category: "Category B", price: 20, timestamp: "2023-01-04" },
+  { itemName: "Item 5", category: "Category A", price: 8, timestamp: "2023-01-05" }
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result);
