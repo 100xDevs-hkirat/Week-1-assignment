e31bf4f4f2887caeb441a65a3ba806a1d8c421bd
Vasu <vasujohri016@gmail.com>
2023-06-16 00:12:13 +0530

    4-promise-chain.js problem solved

--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -6,17 +6,41 @@
  */
 
 function waitOneSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 1 resolve in 1 second");
+        },1*1000)
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 2 resolve in 2 second");
+        },2*1000)
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 3 resolve in 3 second");
+        },3*1000)
+    })
 }
 
 function calculateTime() {
-
+    const startTime = performance.now();
+    waitOneSecond().then(()=>{
+        return waitTwoSecond();
+    }).then(()=>{
+        return waitThreeSecond();
+    }).then(()=>{
+        const endTime = performance.now();
+        const duration = (endTime - startTime)/1000 ;
+        console.log(` ${duration} seconds required to resolve all the three promises`);
+        console.log(`the difference between 3-promise-all.js and 4-promise-chain.js is : ${duration-3.0147945997714998} sec`)
+    })
 }
+
+calculateTime();
