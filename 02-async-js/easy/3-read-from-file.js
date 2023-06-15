ccbacd7c2206d9a20f06851a9f5da372454a5f4e
Arshad-Aly <132923076+Arshad-Aly@users.noreply.github.com>
2023-06-15 15:40:06 +0530

    Completed 3-read-from-file.js

--- 02-async-js/easy/3-read-from-file.js
+++ 02-async-js/easy/3-read-from-file.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
 
+// Read file contents
+fs.readFile('./4-write-to-file.md', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+
+  // Print file contents
+  console.log(data);
+
+  // Perform an expensive operation
+  performExpensiveOperation();
+});
+
+function performExpensiveOperation() {
+  // Simulate an expensive operation
+  for (let i = 0; i < 100000000; i++) {
+    // Perform some computation
+  }
+
+  console.log('Expensive operation completed.');
+}
