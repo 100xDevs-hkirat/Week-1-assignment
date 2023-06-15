8783da75b3718422d472fd78a474e516d3e248c6
Nitin Kalra <nikkalra88@gmail.com>
2023-06-15 18:30:30 +0530

    02-async-js: hard: 4-promise-chain

--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -6,17 +6,38 @@
  */
 
 function waitOneSecond() {
-
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(), 1000);
+	});
 }
 
 function waitTwoSecond() {
-
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(), 2000);
+	});
 }
 
 function waitThreeSecond() {
-
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(), 3000);
+	});
 }
 
 function calculateTime() {
+	const promiseStartTime = new Date();
 
+	waitOneSecond()
+		.then(() => waitTwoSecond())
+		.then(() => waitThreeSecond())
+		.then(() => {
+			const promiseSettledTime = new Date();
+
+			console.log(
+				"All promises are settled, it took " +
+					(promiseSettledTime.getTime() - promiseStartTime.getTime()) / 1000 +
+					"seconds"
+			);
+		});
 }
+
+calculateTime();
