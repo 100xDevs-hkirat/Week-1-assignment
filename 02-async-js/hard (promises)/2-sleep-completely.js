853c6bf4584c345bba2ec194b8d53a6cc60c6e1a
Shivam Agarwal <itisshivam22@gmail.com>
2023-06-15 20:03:57 +0530

    feat: solve sleep completely problem

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,7 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  const start = Date.now();
+  while (Date.now() - start < seconds) {}
 }
