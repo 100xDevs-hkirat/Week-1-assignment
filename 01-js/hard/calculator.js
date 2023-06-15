f91c2c8885c24a9f1b9131c2b3dfc34951f5b10b
Chandan Gupta <94785991+chandanpc@users.noreply.github.com>
2023-06-15 20:01:56 +0530

    Update calculator.js
    calculator function

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,44 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+ constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    const parsedExpression = expression.replace(/\s+/g, '');
+
+    if (!/^[0-9+\-*/().\s]+$/.test(parsedExpression)) {
+      throw new Error('invalid non-numerical characters');
+    }
+
+        this.result = eval(parsedExpression);
+   
+  }}
 
 module.exports = Calculator;
