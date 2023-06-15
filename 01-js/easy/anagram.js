2834dae1192c0f0566f24b4b1c7428b84117fd2f
= <=>
2023-06-15 17:24:24 -0400

    easyProb

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,18 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
+function sort(str) {
+  let arr = str.toLowerCase().split("");
+  let sort = arr.sort().join();
+  return sort;
+}
 
 function isAnagram(str1, str2) {
-
+  if (sort(str1) == sort(str2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
