f1836163461647b4effaf69720edfd43b0272f5a
pavannagulla <pavankalyannagulla28@gmail.com>
2023-06-15 19:39:43 +0530

    01-js problems solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,22 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
+function sortarray(str){
+  var array = str.split("")
+  array = array.sort();
+  var sortedarray = array.join("")
+  return sortedarray
+}
 function isAnagram(str1, str2) {
+  if (sortarray(str1) == sortedarray(str2)){
+    return true;
+  }else{
+    return false;
+  }
 
 }
+
+var ans = isAnagram("pavan","napav");
+console.log(ans);
 
 module.exports = isAnagram;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,45 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    } else {
+      throw new Error('Invalid index');
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    } else {
+      throw new Error('Invalid index');
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } else {
+      throw new Error('Invalid index');
+    }
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
