d9391b9029430de26d880322288df8cd30a1b434
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-06-15 09:40:34 +0530

    1st updated

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,46 @@
 */
 
 class Todo {
-  let
+  constructor(){
+    this.arr = []
+  }
+  add(todo){
+    this.arr.push(todo)
+  }
+  remove(indexOfTodo){
+    if(indexOfTodo>this.arr.length-1) return this.arr
+    let res = []
+    let j =0
+    for (let i = 0; i < this.arr.length-1;i++) {
+      if(i === indexOfTodo) j++
+      res[i] = this.arr[j]
+      j++
+    }
+    this.arr = res
+  }
+  update(index, updateToDo){
+    if(index>this.arr.length-1) return
+    this.arr[index] = updateToDo
+  }
+  get(indexOfTodo){
+    if(indexOfTodo>this.arr.length-1) return null
+    return this.arr[indexOfTodo]
+  }
+  getAll(){
+    return this.arr;
+  }
+  clear(){
+    this.arr = []
+  }
 }
+const ans = new Todo()
+ans.add('hi')
+ans.add('yo')
+ans.add('hello')
+console.log(ans.getAll())
+ans.remove(1)
+console.log(ans.getAll())
+ans.remove(0)
+console.log(ans.getAll())
 
 module.exports = Todo;
