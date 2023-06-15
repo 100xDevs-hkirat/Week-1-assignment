759fa11686ed90667574e21593cd3cb5be0db491
Vasu <vasujohri016@gmail.com>
2023-06-15 23:59:14 +0530

    3-promise-all.js problem solved

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,37 @@
 
 
 function waitOneSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 1 resolve in 1 second");
+        },1*1000)
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 2 resolve in 2 second");
+        },2*1000)
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve("Promise 3 resolve in 3 second");
+        },3*1000)
+    })
 }
 
 function calculateTime() {
-
+    const startTime = performance.now();
+    Promise.all([waitOneSecond(),waitTwoSecond(),waitThreeSecond()]).then(()=>{
+        const endTime = performance.now();
+    const duration = (endTime - startTime)/1000 ;
+    console.log(` ${duration} seconds required to resolve all the three promises`);
+    })
+    
 }
+
+calculateTime();
