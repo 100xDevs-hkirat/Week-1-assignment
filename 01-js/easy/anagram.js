7591052a344e200964668fa7c401109b5a11d4ae
iMandarin <odhamdhere@gmail.com>
2023-06-15 14:12:20 +0530

    Anagram program solved.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,14 +1,28 @@
 /*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  Write a function `isAnagram` which takes 2 parameters.
+  and returns true/false if those are anagrams or not.
+
   What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+  - A word, phrase, or name formed by rearranging the letters of another,
+  such as spar, formed from rasp.
 
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
 
 function isAnagram(str1, str2) {
+  if (str1.split("").sort().join("")== str2.split("").sort().join(""))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 
 }
 
-module.exports = isAnagram;
+//module.exports = isAnagram;
+
+let answer = isAnagram("spar", "raap");
+console.log(answer);
