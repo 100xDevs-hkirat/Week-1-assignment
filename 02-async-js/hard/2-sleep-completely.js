9c6ec6976aa9810daa0bb0c261876e5c0713c0fa
Mudit <muditert34@gmail.com>
2023-06-16 23:15:34 +0530

    promise-all.js added

--- 02-async-js/hard/2-sleep-completely.js
+++ 02-async-js/hard/2-sleep-completely.js
@@ -3,6 +3,6 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
+function sleep(seconds) {
 
 }
--- 02-async-js/hard/3-promise-all.js
+++ 02-async-js/hard/3-promise-all.js
@@ -6,17 +6,39 @@
 
 
 function waitOneSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 1 resolved successfully')
+        }, 1000)
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 2 resolved successfully')
+        }, 2000)
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 3 resolved successfully')
+        }, 3000)
+    })
 }
 
 function calculateTime() {
-
+    return Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()])
+        .then((res) => {
+            const end = Date.now()
+            console.log('Promise resolved successfully after ', ((end - start) / 1000))
+        })
+        .catch(err => {
+            console.log('Failed to resolve promise')
+        })
 }
+
+const start = Date.now()
+calculateTime()
