81cc95d7d5e41518b3684d1fb0e291a482b10e8d
Yugal Kumar Singh <113284190+YugalSingh1234@users.noreply.github.com>
2023-06-17 02:24:28 +0530

    Updated anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,24 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+// program 1st of Assigment
 
+function isAnagram(str1, str2) {   
+   let x = str1.split('').sort().join('');
+   let y = str2.split('').sort().join('');
+ 
+  /* first splitiing the element and making it an Array then sorting 
+  it in alphawordical order and then joining it and making it an string and 
+  at last equating it*/  
+  
+   if(x === y){
+      console.log("True");
+   } else { 
+      console.log("False");
+   }
 }
-
+// for false response
+isAnagram("india","ndiani")
+// for true response
+isAnagram("yugal","uylga")
 module.exports = isAnagram;
