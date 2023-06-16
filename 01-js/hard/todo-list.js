49ff4ccd8581d66714139e60d98c58f0e2949c90
Vasu <vasujohri016@gmail.com>
2023-06-17 02:06:00 +0530

    Hard Problem set done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,35 @@
 */
 
 class Todo {
+  todos = []
 
+  add(todo) {
+    this.todos.push(todo)
+  }
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1)
+  }
+  update(index, updatedTodo) {
+    if (index < this.todos.length - 1) {
+      this.todos.splice(index, 1, updatedTodo)
+    }
+  }
+  getAll() {
+    return this.todos
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo > this.todos.length - 1) {
+      return null
+    }
+    return this.todos[indexOfTodo]
+  }
+  clear() {
+    this.todos = []
+  }
 }
+
+let t = new Todo()
+t.update(2, "Hello")
+console.log(t.getAll())
 
 module.exports = Todo;
