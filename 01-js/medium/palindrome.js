9b6d2467badc4fa1b57bc0ed350767a8768d9153
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-06-16 23:51:04 +0530

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,24 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
-
+function reverse(str){
+  let ans="";
+  for(let i=str.length-1;i>=0;i--){
+    ans+=str[i];
+  }
+  return ans;
+}
 function isPalindrome(str) {
-  return true;
+  str=str.toLowerCase();
+// str1=str.split("").reverse().join("");
+let str1=reverse(str);
+if(str1===str)return true;
+else{
+  return false;
 }
 
-module.exports = isPalindrome;
+}
+
+// module.exports = isPalindrome;
+let ans=isPalindrome("Madam")
+console.log(ans);
