cd13c2f06463954aa6b83c7c3c4145a0f70c8bab
Varsha-dr <114489823+varshadr@users.noreply.github.com>
2023-06-16 15:05:35 +0530

    Implemented isAnagram function

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,19 @@
 */
 
 function isAnagram(str1, str2) {
+  const len1 = str1.length;
+  const len2 = str2.length;
 
+  if (len1 !== len2) {
+    return false; // If the lengths are different, they can't be anagrams
+  }
+
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
+
+console.log(isAnagram("varsha", "ashrav")); 
 
 module.exports = isAnagram;
