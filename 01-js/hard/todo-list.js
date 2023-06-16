be6958977aff43f8a869d9ef0ac41617881f6312
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-06-17 02:06:22 +0530

    TodoList

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,37 @@
 */
 
 class Todo {
-
+constructor(){
+  this.todos=[]; // array that stores our data in memory
 }
+add(newTodo) {
+  this.todos.push(newTodo);
+}
+remove(indexOfTodo){
+  if (indexOfTodo>=0 && this.todos.length>indexOfTodo && this.todos[indexOfTodo]){
+    this.todos.splice(indexOfTodo,1);
+}
+}
+update(indexOfTodo,updatedTodo){
+  if(indexOfTodo >= 0 && indexOfTodo < this.todos.length){
+    this.todos[indexOfTodo]=updatedTodo;
+  }
+}
+getAll(){
+  return this.todos;
+}
+clear(){
+ this.todos=[];
+} 
+}
+let todo=new Todo();
+todo.add("TSOC plugin research");
+todo.add("End term Exam");
+todo.add("Cohort Assignment");
+todo.add("DSA");
+// todo.remove(2);
+// todo.clear();
+console.log(todo.getAll());
 
-module.exports = Todo;
+
+// module.exports = Todo;
