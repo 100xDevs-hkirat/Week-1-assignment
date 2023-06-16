4ae38bf2be67a552af81a93d1f59bf869b76d64e
Rishabh Pandey <rishabh@Rishabhs-MacBook-Air.local>
2023-06-16 22:14:31 +0530

    only solved some hard promise problems

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,34 @@
 
 
 function waitOneSecond() {
+    let firstPromise = new Promise((resolve)=>setTimeout(resolve,1000));
+    
+    return firstPromise;
 
 }
 
 function waitTwoSecond() {
-
+    let secondPromise = new Promise((resolve)=>setTimeout(resolve,2*1000));
+    
+    return secondPromise;
 }
 
 function waitThreeSecond() {
+
+    let thirdPromise = new Promise((resolve)=>setTimeout(resolve,3*1000));
+    
+    return thirdPromise;
 
 }
 
 function calculateTime() {
 
+    return Promise.all([waitOneSecond,waitTwoSecond,waitThreeSecond])
+
 }
+
+waitOneSecond().then(()=>console.log("one"))
+waitTwoSecond().then(()=>console.log("two"))
+waitThreeSecond().then(()=>console.log("three"))
+
+calculateTime().then(()=>console.log("total time"))
