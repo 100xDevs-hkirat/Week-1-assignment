83f000d4ee325bee6c91628b605127363168da5c
syedscodingden <syedscodingden@gmail.com>
2023-06-17 11:54:12 +0530

    first commit

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,11 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  if(str.toLowerCase().replace(/[^a-z]/g, "") === str.toLowerCase().replace(/[^a-z]/g, "").split("").reverse().join("")) {
+    return true;
+  }
+  return false;
 }
 
+// console.log(isPalindrome("!baab!!"));
 module.exports = isPalindrome;
