ee79015b7d0668c93adb4657987be11f74c193f6
amar26 <amarps12345@gmail.com>
2023-06-17 11:07:25 +0530

    Solved easy problems

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,34 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  let summary = [];
 
+  for(var i=0; i<transactions.length; i++){
+    
+    let index = -1;
+    for(var j=0; j<summary.length; j++){
+      if(summary[j].category === transactions[i].category){
+        index =  j;
+        summary[j].total_amount_spend += transactions[i].price;
+      }
+    }
+    if (index == -1){
+      let t = {'category':transactions[i].category, 'total_amount_spend':transactions[i].price};
+      summary.push(t);
+    }
+  }
+
+//  console.log(summary);
+  return summary;
+}
+/*
+let obj = [{ itemName:'Soap', category:'HouseHold', price:60 },
+{ itemName:'Burger', category:'Food', price:110 },
+{ itemName:'Ketchup', category:'HouseHold', price:500 },
+{ itemName:'Bike Repair', category:'Vehicle', price:600 },
+{ itemName:'Petrol', category:'Vehicle', price:12000 },
+{ itemName:'Internet', category:'HouseHold', price:60 }];
+
+console.log(calculateTotalSpentByCategory(obj));
+*/
 module.exports = calculateTotalSpentByCategory;
