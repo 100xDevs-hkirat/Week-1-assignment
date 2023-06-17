d6b0bcbe1233f397fad1ea585a4a4b0db4d8d033
Meet Vekaria <63900623+MeetVekaria@users.noreply.github.com>
2023-06-17 12:44:19 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,11 +4,11 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 
   Once you've implemented the logic, test your code by running
-  - `npm run test-anagram`
+  - 'node anagram.js`
 */
 
 function isAnagram(str1, str2) {
 
 }
 
-module.exports = isAnagram;
+console.log(isAnagram('asap', 'pasa');
