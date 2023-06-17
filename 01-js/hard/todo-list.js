633a612ac44165e3195bdf7a0c5fb44c321f1de3
rain2003 <vgssam2003@gmail.com>
2023-06-17 18:10:45 +0530

    solved js hard question

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,8 +11,41 @@
   - `npm run test-todo-list`
 */
 
-class Todo {
+class Todo{
+  constructor(){
+    this.arr = [];
+  }
+
+  add (todo){
+    this.arr.push(todo);
+  }
+
+  remove(indexOfTodo){
+    if ( indexOfTodo >= 0 && indexOfTodo < this.arr.length){
+      this.arr.splice(indexOfTodo,1);
+    }
+  }
+
+  update(index, updatedTodo){
+    if ( indexOfTodo >= 0 && indexOfTodo < this.arr.length){
+      this.arr[index] = updatedTodo;
+    }
+  }
+  
+  getAll(){
+    return this.arr;
+  }
+
+  get(indexOfTodo){
+    return this.arr[indexOfTodo];
+  }
+
+  clear(){
+    this.arr= [];
+  }
 
 }
+
+
 
 module.exports = Todo;
