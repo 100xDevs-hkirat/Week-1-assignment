5a6a62bb5004049f0fcd998fef776eeeab10dae0
Aman Prasad <amanprasad@Amans-MacBook-Pro-m2.local>
2023-06-19 20:23:51 +0530

    anagrams

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,21 @@
 */
 
 function isAnagram(str1, str2) {
-
+  var hashedStr1 = generateHash(str1);
+  var hashedStr2 = generateHash(str2);
+  if(hashedStr1 === hashedStr2){
+    return true;
+  }
+  return false;
 }
 
-module.exports = isAnagram;
+function generateHash(str){
+  const array = new Array(26).fill(0);
+  for(var i=0; i<str.length; i++){
+    array[str[i].charCodeAt(0)-'a'.charCodeAt(0)] += 1;
+  }
+  //console.log(array);
+  return array.join("#");
+}
+
+console.log(isAnagram("card", "racd"));
