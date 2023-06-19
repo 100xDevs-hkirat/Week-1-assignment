edeb360cbadcdd823e0c437f84203388da0c5d46
risarora <risarora@gmail.com>
2023-06-20 00:06:40 +0530

    Fix Body Parser and Middleware

--- Week_02_NodeJs_Backend_systems_HTTP_Servers/day-2/body-parser.js
+++ Week_02_NodeJs_Backend_systems_HTTP_Servers/day-2/body-parser.js
@@ -3,14 +3,7 @@ const bodyParser = require("body-parser")
 
 const app = express();
 const port = 3000;
-/*
-function middleWare(req, res, next) {
-  console.log("from inside middleware " + req.headers.counter);
-  next()
-}
 
-app.use(middleWare);
-*/
 app.use(bodyParser.json())
 function calculateSum(counter) {
   let sum = 0;
@@ -22,14 +15,14 @@ function calculateSum(counter) {
 }
 
 function confirmServerStarted() {
-  console.log("Server Started ");
+  console.log("Server Started at ", port);
 }
 app.listen(port, confirmServerStarted);
 
 function processSum(req, res) {
-  console.log("req.query" + req.query.toString)
-  console.log("req.headers" + req.headers)
-  console.log("req.body" + req.body)
+  console.log("req.query" , req.query)
+  console.log("req.headers" , req.headers)
+  console.log("req.body" , req.body)
   const counter = req.body.counter;
   console.log("counter is "+ counter)
   var sum = calculateSum(counter)
--- Week_02_NodeJs_Backend_systems_HTTP_Servers/day-2/middleware.js
+++ Week_02_NodeJs_Backend_systems_HTTP_Servers/day-2/middleware.js
@@ -1,46 +1,52 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-/*
+
+// Middleware function
 function middleWare(req, res, next) {
   console.log("from inside middleware " + req.headers.counter);
-  next()
+  next();
 }
 
 app.use(middleWare);
-*/
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 function calculateSum(counter) {
   let sum = 0;
-  for (var i=0; i < counter; i++) {
+  for (let i = 0; i <= counter; i++) {
     sum += i;
   }
-  console.log("Sum in coaculateSum "+sum )
+  console.log("Sum in calculateSum: " + sum);
   return sum;
 }
 
 function confirmServerStarted() {
-  console.log("Server Started ");
+  console.log("Server Started");
 }
+
 app.listen(port, confirmServerStarted);
 
 function processSum(req, res) {
-  console.log("req.query" + req.query)
-  console.log("req.headers" + req.headers)
-  console.log("req.body" + req.body)
+  console.log("req.query", req.query);
+  console.log("req.headers", req.headers);
+  console.log("req.body", req.body);
+  
   const counter = req.body.counter;
-  console.log("counter is "+ counter)
-  var sum = calculateSum(counter)
-  console.log(sum)
-  res.send("Sum is "+sum)
+  console.log("counter is " + counter);
+  
+  const sum = calculateSum(counter);
+  console.log(sum);
+  
+  res.send("Sum is " + sum);
 }
 
 app.post("/getSum", processSum);
 
-function sayHello(){
-  console.log("------------Hello---------")
-  res.send('----------/----------------')
+function sayHello(req, res) {
+  console.log("------------Hello---------");
+  res.send('Hello, World!');
 }
-app.get('/', sayHello)
+
+app.get('/', sayHello);
