5405720ab2d8679943caa0e6ffddd466740179ae
Akash Singh <developer.akash8@gmail.com>
2023-06-19 11:27:28 +0530

    Implemented class for todo-list

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,42 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    if (!this.todos.includes(todo)){
+      this.todos.push(todo);
+    }
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    }
+    return null;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
