4258b35cce499ddc0f20b494b14269efd3c6384b
amitomksp <112419542+amitomksp@users.noreply.github.com>
2023-06-23 18:25:01 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+  var string1=str1.toUppercase();
+  var string2=str2.toUppercase();
+if(string1.sort()==string2.sort()){
+  return true;}
+  else{
+    return false;
+  }
 }
+var answer=isAnagram("Amit","mAIt");
+console.log(answer);
 
 module.exports = isAnagram;
