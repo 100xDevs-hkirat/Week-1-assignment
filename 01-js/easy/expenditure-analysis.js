df421978da301fd93b9e2b6218e373ce805de7d6
Yogesh <a.yogesh87@gmail.com>
2023-07-11 21:52:22 +0530

    expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,75 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  let totalByCategory = {};
+
+  for(let i = 0; i < transactions.length; i++)
+  {
+    let item = arr[i];
+    let category = item.category;
+    let price = item.price;
+
+    if(totalByCategory[category])
+    {
+      totalByCategory[category] += Math.round(price);
+    }
+    else
+    {
+      totalByCategory[category] = Math.round(price);
+    }
+  }
+  
+  return totalByCategory;
 }
+
+const arr = [
+  {
+    itemName: "item1",
+    category: "category1",
+    price: 55.6,
+    timestamp: "2023-07-07"
+  },
+  {
+    itemName: "item2",
+    category: "category2",
+    price: 73,
+    timestamp: "2023-07-08"
+  },
+  {
+    itemName: "item3",
+    category: "category1",
+    price: 84.3,
+    timestamp: "2023-07-09"
+  },
+  {
+    itemName: "item4",
+    category: "category1",
+    price: 99.2,
+    timestamp: "2023-07-10"
+  },
+  {
+    itemName: "item5",
+    category: "category2",
+    price: 34.7,
+    timestamp: "2023-07-12"
+  },
+  {
+    itemName: "item6",
+    category: "category3",
+    price: 75.33,
+    timestamp: "2023-07-13"
+  },
+  {
+    itemName: "item4",
+    category: "category2",
+    price: 24.1,
+    timestamp: "2023-07-10"
+  }
+];
+
+const total_amount_spent = calculateTotalSpentByCategory(arr);
+
+console.log(total_amount_spent);
 
 module.exports = calculateTotalSpentByCategory;
