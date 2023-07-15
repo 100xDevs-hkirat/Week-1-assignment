aa774e912261ddabe3bd8c5887f4c0a3dd3e10fd
Tanay Kumar <111867105+Tanaykmr@users.noreply.github.com>
2023-07-15 18:59:18 +0530

    Updated anagram.js
    I have a lot of backlog in the course because my end sem exams popped up. Gonna speedrun now, so you'll be seeing a lot of PR's from em in the upcoming days.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+var ans1 = str1.split(''); //we convert the string str1 to an object, so that we can apply the .sort() operation on it
+  ans1.sort();
+  ans1 = ans1.join(''); //we  now convert the ans1 of type object back to a string
+  var ans2 = str2.split('');
+  ans2.sort();
+  ans2 = ans2.join('');
+  if (ans1 == ans2)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
