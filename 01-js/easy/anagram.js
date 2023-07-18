74af9ab011d2a737fa37a6619ceec33bbf0bb612
Vibhor <vibgitcode27@gmail.com>
2023-07-18 23:58:44 +0530

    Solved few question

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,34 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let st1 = str1.toLowerCase().split(``)
+  let st2 = str2.toLowerCase().split(``)
+          st1.sort();
+          st2.sort();
+    let j = 0;
+    if(st1.length == st2.length)
+    {
+      for(let  i = 0 ; i < st1.length ; i++)
+        {
+          if(st1[i] != st2[i])
+          {
+            return false
+            break;
+          }
+          else
+          {
+            j++
+          }
+        }
+      }
+    else
+    {
+      return false
+    }
+    if(j==st1.length)
+    {
+      return true
+    }
 }
-
+isAnagram('School MASTER', 'The ClassROOM')
 module.exports = isAnagram;
