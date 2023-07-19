fc6416f96f9b5bd457067d3576d9c1e5ed206f3a
kaulmesanyam <kaulsanyam@gmail.com>
2023-07-19 16:45:08 +0530

    adding description to fileCleaner.js

--- 02-async-js/medium/fileCleaner.js
+++ 02-async-js/medium/fileCleaner.js
@@ -7,7 +7,13 @@ fs.readFile("./file-to-clean.txt", 'utf8', (err, data) => {
     } else {
         let cleanedData = data.replace(/\s+/g, " ").trim();
         console.log("Data read from file successfully");
-
+        
+        /*its important for the file writing logic to be present here inside the
+        readFile callback function as we need to make the file AFTER the data has
+        been read form the other file. If we put this writing logic outside readFile()
+        then writeFile() will create the file which will be empty as readFile prcess would 
+        not be completed in time.
+        */
         fs.writeFile("cleanedFile.txt",cleanedData, 'utf8', (err) => {
             if(err) {
                 throw new Error("Error occured while writing data into file");
