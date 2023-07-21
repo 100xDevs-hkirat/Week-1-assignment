9a9df3fdc0e5fb0f4c3030034801cf41c7f5c843
Chirag Vasant Patil <70964754+chirag4242@users.noreply.github.com>
2023-07-20 21:11:32 -0600

    Add calculateTime solution

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const startTime = new Date();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const endTime = new Date();
+    const calculatedTime = (endTime - startTime) / 1000;
+    return calculatedTime;
 }
