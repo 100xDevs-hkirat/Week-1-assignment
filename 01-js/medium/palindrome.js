ac5c5b0f1430a5dba792da468406e8f3e7a3af33
Chirag Vasant Patil <70964754+chirag4242@users.noreply.github.com>
2023-07-20 20:13:15 -0600

    add isPalindrome solution

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,11 @@
 */
 
 function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i].toLowerCase() !== str[str.length - i - 1].toLowerCase()) {
+      return false;
+    }
+  }
   return true;
 }
 
