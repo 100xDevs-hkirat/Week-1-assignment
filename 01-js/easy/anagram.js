a6f2cc239d908d22b7d78c306d0337f27909db7b
Pratham SIkka <116445216+PrathamSikka24@users.noreply.github.com>
2023-08-04 09:05:40 +0530

    Solved the anagram.js question from assignment week1_easy.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,49 @@
 */
 
 function isAnagram(str1, str2) {
+  if (length.str1==length.str2) {
+    
+  }
+  }
+  
+  module.exports = isAnagram;
 
+
+
+
+function isAnagram(str1, str2) {
+  // Check if the lengths of the strings are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // Convert the strings to lowercase to ignore case sensitivity
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  // Create character frequency maps for both strings
+  const charMap1 = {};
+  const charMap2 = {};
+
+  // Populate the character frequency maps
+  for (let char of str1) {
+    charMap1[char] = (charMap1[char] || 0) + 1;
+  }
+
+  for (let char of str2) {
+    charMap2[char] = (charMap2[char] || 0) + 1;
+  }
+
+  // Compare the character frequency maps
+  for (let char in charMap1) {
+    if (charMap1[char] !== charMap2[char]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
+const result = isAnagram("paws", "swap");
+console.log(result); // Output: true
