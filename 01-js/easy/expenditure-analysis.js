ff7e2bbdd49cb849a9e11132f4d31cee9f2081b2
Swati <swati.snm20@gmail.com>
2023-08-05 23:20:46 +0530

    expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,52 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  /**
+   * const transactions = [
+			{
+				id: 1,
+				timestamp: 1656076800000,
+				price: 10,
+				category: 'Food',
+				itemName: 'Pizza',
+			},
+			{
+				id: 2,
+				timestamp: 1656259600000,
+				price: 20,
+				category: 'Food',
+				itemName: 'Burger',
+			},
+			{
+				id: 3,
+				timestamp: 1656019200000,
+				price: 15,
+				category: 'Clothing',
+				itemName: 'T-Shirt',
+			}
+    ]
+   */
+
+    const ansMap = new Map();
+for (var key in transactions) {
+  var val = transactions[key];
+  if (ansMap.has(val.category)) {
+    var val2 = ansMap.get(val.category);
+    ansMap.set(val.category, val2+val.price);
+  } else {
+    ansMap.set(val.category, val.price);
+  }
+}
+const ans = [];
+for (const [key, value] of ansMap.entries()) {
+  console.log(key, value);
+  ans.push({
+    "category": key,
+    "totalSpent":value
+  });
+}
+
+return ans;
 }
 
 module.exports = calculateTotalSpentByCategory;
