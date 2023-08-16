76bf96c3ae4ff4d8a1bd145207fbb53b049efc06
Ahesh-A <21pa01@psgtech.ac.in>
2023-08-16 23:45:20 +0530

    implemented times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let start =  Date.now();
+    let sum = 0;
+    for(let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    let end =  Date.now();
+
+    return (end - start) / 1000;
 }
