650386f9f336a8d01c2c1420463d47c033e7c0a8
Yash Budhia <142312760+yashbudhia@users.noreply.github.com>
2023-08-18 00:20:10 +0530

    Anagram assignment code written

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,25 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
 function isAnagram(str1, str2) {
+    let S1 = str1.split('');
+    let S2 = str2.split('');
+    if (S1.length !== S2.length)
+    { return false ; }
+    else {
+        let n = S1.length ;
+        for ( i = 0; i < n ; i++)
+        {
+           for ( j= 0; j <n ; j++)
+           {
+                if ( S1[i] == S2[j])
+               { return true;
+               }
+               break;
+            }
 
+    }
+    }
 }
 
 module.exports = isAnagram;
