85f483306e1885bed285a896cfd3f6c734a4fa8b
Neeru Kapoor <467neeru@gmail.com>
2023-08-23 00:12:05 +0530

    Expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,63 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  const myMap = new Map();
+  for(let i=0;i<transactions.length;i++) {
+    if(myMap.has(transactions[i].category)) {
+      const value = myMap.get(transactions[i].category) + transactions[i].price 
+      myMap.set(transactions[i].category, value);
+    }
+    else 
+      myMap.set(transactions[i].category, transactions[i].price)
+  }
+  const ans = []
+
+  for (const [key, value] of myMap.entries()) {
+    ans.push({ [key]: value });
+  }
+  return ans;
 }
+
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
+
+const result = calculateTotalSpentByCategory(transactions)
+console.log(result)
 
 module.exports = calculateTotalSpentByCategory;
