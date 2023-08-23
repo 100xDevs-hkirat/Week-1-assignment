d3d2c00ff8db46e6ea16fce1a8867e62aea85bc5
yashbudhia <yeshbudhia@gmail.com>
2023-08-23 22:35:22 +0530

    week- 1 part -1

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,45 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result =0;
+  }
+  add(number) {
+    this.result += number ; 
+  }
+  subtract(number) {
+    this.result -+ number ;
+  }
+  multiply(number) {
+      this.result *= number ;
+    }
+  divide(number) {
+    if(number !== 0) {
+      this.result /= number ;
+    }
+    else {
+      throw new Error("Division by zero is not possible");
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result ;
+  }
+  calculate(expression) {
+    expression = expression.replace(/\s + /g , '');
+  if(! /^[0-9+\-*/().\s]*$/.test(expression)){
+    throw new Error("Only Numbers and arithmetic operators Allowed")
+  }
+  try{
+    this.result = eval(expression);
+  }
+  catch(error) {
+    throw new Error("Something is not right");
+  }
 
+  }
+  }  
 module.exports = Calculator;
