a19a1196e25a6afa68926e326e5f739f1ded5f11
PrasadPonnada <PrasadPonnada09@gmail.com>
2023-08-26 13:41:14 +0530

    01-Js Assisnments completed

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,42 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.toDoList = [];
+  }
+  add(todo) {
+    this.toDoList.push(todo);
+  }
+  remove(index) {
+    this.toDoList.splice(index, 1);
+  }
+  update(index, todo) {
+    this.toDoList[index] = todo;
+  }
+  getAll() {
+    return this.toDoList;
+  }
+  get(index) {
+    return this.toDoList[index];
+  }
+  clear() {
+    this.toDoList = [];
+  }
 }
 
+const todoObj = new Todo();
+todoObj.add("List1");
+todoObj.add("List2");
+todoObj.add("List3");
+
+todoObj.update(0, "Updated");
+todoObj.remove(0);
+
+let list = todoObj.getAll();
+console.log(list);
+
+let item = todoObj.get(1);
+console.log(item);
+
+todoObj.clear();
 module.exports = Todo;
