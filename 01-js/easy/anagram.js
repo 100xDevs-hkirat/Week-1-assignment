9e59c82c0b1fb4c570dd1e2c68d16e035774f431
Krishna Mahalka <119232004+KrishnaMahalka@users.noreply.github.com>
2023-09-01 13:42:07 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,27 @@
 */
 
 function isAnagram(str1, str2) {
-
+var arr1=str1.split("");
+   var arr2=str2.split("");
+   var n1= arr1.length;
+   var n2=arr2.length;
+   var arr3=[];
+   if(n1===n2){
+    for(var i=0;i<n1;i++){
+        for(j=0;j<n2;j++){
+            if(arr1[i]===arr2[j]){
+                arr3.push(arr1[i]);
+            }
+        }
+    }
+    if(arr3.length===n1){
+        return true;
+    }
+   }
+   else{
+    return false
+   }
+   return false;
 }
 
 module.exports = isAnagram;
