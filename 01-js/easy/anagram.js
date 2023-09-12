a3094aa50557e9c950ce2e491434ffd0673644a4
Satwant Singh <sahib@Satwants-MacBook-Air.local>
2023-09-13 01:39:41 +0530

    isAnagram funciton changes

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,18 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
 function isAnagram(str1, str2) {
-
+    var indexArray =  Array(128).fill(0)
+    if(str1.length != str2.length)  return false
+    for(var i=0;i<str1.length;i++){
+        var charIndex1 = str1.charCodeAt(i)
+        indexArray[charIndex1]++;
+        var charIndex2 = str2.charCodeAt(i)
+        indexArray[charIndex2]--;
+    }
+    for(var i=0;i<indexArray.length;i++){
+        if(indexArray[i] != 0)  return false;
+    }
+    return true;
 }
-
 module.exports = isAnagram;
