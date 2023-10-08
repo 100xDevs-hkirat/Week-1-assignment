b4ef9b50ec516eb1243ccfdeea5e2717f9d470d8
Parve Palial <42088698+ParvePalial@users.noreply.github.com>
2023-10-08 22:55:52 +0530

    Update anagram.js
    Here's My Solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+    var a=0,b=0;
+    if (str1.length == str2.length){
+        for (var j=0; j<str1.length; j++){
+            a += str1.charCodeAt(j);
+            b += str2.charCodeAt(j);
+        }
+        if (a==b) console.log("Yes");
+    }
+    else console.log("No");
 }
 
 module.exports = isAnagram;
