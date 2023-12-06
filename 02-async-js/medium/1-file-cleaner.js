aad38870407c7e4eded7d66a318e641ba42a2dd5
Barath Elangovan <barath_elangovan@outlook.com>
2023-12-06 18:13:19 +0530

    medium tasks

---
+++ 02-async-js/medium/1-file-cleaner.js
@@ -0,0 +1,26 @@
+// ## File cleaner
+// Read a file, remove all the extra spaces and write it back to the same file.
+
+// For example, if the file input was
+// ```
+// hello     world    my    name   is       raman
+// ```
+
+// After the program runs, the output should be
+
+// ```
+// hello world my name is raman
+// ```
+
+const fs = require("fs");
+
+const path = "./text.txt";
+
+fs.readFile(path, 'utf8',(err, data)=>{
+    if(err){
+        console.error(err)
+        retrun;
+    }
+    const modifiedData = data.replace(/\s+/g, ' ');
+    console.log(modifiedData);
+})
