1965ddb92dcb1a2bb3586f9fc3c3afbcd65d2ad9
Suraj kumar <125961509+kumarsuraj212003@users.noreply.github.com>
2024-02-24 00:01:56 +0530

    making anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,19 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
 function isAnagram(str1, str2) {
+  const sortedStr1 = str1.toLowerCase().replace(/\s/g, '').split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().replace(/\s/g, '').split('').sort().join('');
 
+  return sortedStr1 === sortedStr2;
+}
+
+const string1 = "listen";
+const string2 = "silent";
+if (isAnagram(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
 }
 
 module.exports = isAnagram;
