4abbc3e0548c37028a703ad8ca38e7abbd363fab
Kaushikgtm <162317291+Kaushikgtm@users.noreply.github.com>
2024-03-05 21:28:39 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,20 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
+function sort(str){
+  var arr = str.split("");
+ arr =  arr.sort();
+  var sortedarray = arr.join("");
+console.log(sortedarray);
+  return sortedarray;
+}  
 
 function isAnagram(str1, str2) {
-
+if(sort(str1) == sort(str2)){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
